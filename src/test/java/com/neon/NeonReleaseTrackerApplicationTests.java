b43db2bb92cab9fcdc5d95053698834e8d9f9e0d
bojan-92 <bojantrifkovic92@gmail.com>
2023-09-26 15:10:58 +0200

    initial commit

---
+++ src/test/java/com/neon/NeonReleaseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeonReleaseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
